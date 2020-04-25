54d7b87786355a9542ee36ae795b090bc599c8ab
Sandie Nunez <snunez@vt.edu>
2020-04-25 03:01:18 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -12,30 +12,18 @@ class Transfer
     @sender.valid? && @receiver.valid?
   end
 
-def execute_transaction
-    if @sender.balance < @amount
-      @status = "rejected"
-      return "Transaction rejected. Please check your account balance."
 
-    elsif @status == "complete"
-      puts "Transaction was already excuted"
-    else
-      @sender.deposit( @amount * -1 ) 
-      @receiver.deposit( @amount )
+
+  def execute_transaction
+    if @sender.balance > @amount && @status == "pending"
+      @sender.balance -= @amount
+      @receiver.balance += @amount
       @status = "complete"
+    else
+      @status = "rejected"
+    "Transaction rejected. Please check your account balance."
     end
   end
-
-  # def execute_transaction
-  #   if @sender.balance > @amount && @status == "pending"
-  #     @sender.balance -= @amount
-  #     @receiver.balance += @amount
-  #     @status = "complete"
-  #   else
-  #     @status = "rejected"
-  #   "Transaction rejected. Please check your account balance."
-  #   end
-  # end
 
   def reverse_transfer
     if @status == "complete"
